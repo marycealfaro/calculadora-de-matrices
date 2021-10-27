b8979e334a84ea73afe028f9ed0e8df8ab0375e9
marycealfaro <91059694+marycealfaro@users.noreply.github.com>
2021-10-27 04:52:40 -0600

    confi buttom

--- calculadora de matrices/Form1.Designer.cs
+++ calculadora de matrices/Form1.Designer.cs
@@ -28,12 +28,192 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1crearmatriz = new System.Windows.Forms.Button();
+            this.button2crearmatriz = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gabriola", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(342, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(293, 60);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculadora de matrices";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = " Eliga el tamaño de su matriz";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Matriz 1 ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(90, 127);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(32, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(128, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "x";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(90, 242);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(32, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 249);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Matriz 2";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(148, 244);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(32, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(148, 127);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(32, 22);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(128, 247);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "x";
+            // 
+            // button1crearmatriz
+            // 
+            this.button1crearmatriz.Location = new System.Drawing.Point(228, 125);
+            this.button1crearmatriz.Name = "button1crearmatriz";
+            this.button1crearmatriz.Size = new System.Drawing.Size(106, 31);
+            this.button1crearmatriz.TabIndex = 11;
+            this.button1crearmatriz.Text = "Crear Matriz";
+            this.button1crearmatriz.UseVisualStyleBackColor = true;
+            this.button1crearmatriz.Click += new System.EventHandler(this.button1crearmatriz_Click);
+            // 
+            // button2crearmatriz
+            // 
+            this.button2crearmatriz.Location = new System.Drawing.Point(228, 235);
+            this.button2crearmatriz.Name = "button2crearmatriz";
+            this.button2crearmatriz.Size = new System.Drawing.Size(106, 31);
+            this.button2crearmatriz.TabIndex = 12;
+            this.button2crearmatriz.Text = "Crear Matriz";
+            this.button2crearmatriz.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(360, 106);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(248, 252);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Matriz 1";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(662, 106);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(248, 252);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Matriz 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(983, 534);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2crearmatriz);
+            this.Controls.Add(this.button1crearmatriz);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1crearmatriz;
+        private System.Windows.Forms.Button button2crearmatriz;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
--- calculadora de matrices/Form1.cs
+++ calculadora de matrices/Form1.cs
@@ -12,9 +12,98 @@ namespace calculadora_de_matrices
 {
     public partial class Form1 : Form
     {
+        private TextBox[,] Matriz1;
+        private TextBox[,] Matriz2;
+        private TextBox[,] Resultante;
+        private float determinante;
+
+        int linea1, columna1;
+        int linea2, columna2;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1crearmatriz_Click(object sender, EventArgs e)
+        {
+            groupBox1.Controls.Clear();
+            /*if (textBox1.Text.Length == 0)
+           {
+               MessageBox.Show("El tamaño de la matriz es nula", "Error");
+               return;
+           }
+           if (textBox3.Text == null)
+           {
+               MessageBox.Show("La columna de la matriz es nula.", "Error");
+               return;
+           }
+           linea1 = Convert.ToInt32(textBox1.Text);
+           columna1 = Convert.ToInt32(textBox3.Text);*/
+            if (!int.TryParse(textBox1.Text, out linea1))
+            {
+                MessageBox.Show("La linea de la matriz 1 es nula.", "Error");
+                return;
+            }
+            if (!int.TryParse(textBox3.Text, out columna1))
+            {
+                MessageBox.Show("La columna de la primera matriz es nula.", "Error");
+                return;
+            }
+
+
+            Matriz1 = new TextBox[linea1, columna1];
+            int TamanhoText = groupBoxMatriz1.Width / coluna1;
+            for (int x = 0; x < Matriz1.GetLength(0); x++)
+            {
+                for (int y = 0; y < Matriz1.GetLength(1); y++)
+                {
+                    Matriz1[x, y] = new TextBox();
+                    Matriz1[x, y].Text = "0";
+                    Matriz1[x, y].Top = (x * Matriz1[x, y].Height) + 20;
+                    Matriz1[x, y].Left = y * TamanhoText + 6;
+                    Matriz1[x, y].Width = TamanhoText;
+                    groupBoxMatriz1.Controls.Add(Matriz1[x, y]);
+                }
+            }
+        }
+    }
+
+
+
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
